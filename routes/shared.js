533a9b55fb86b32785c01f6ad643b5da0324ee86
xCodarus <49170208+xCodarus@users.noreply.github.com>
2020-03-05 20:19:24 -0800

    Add files via upload

--- routes/shared.js
+++ routes/shared.js
@@ -11,9 +11,24 @@ exports.viewProject = function(req, res){
 			var themScore = data.users[i].growth;
 		}
 	}
+	var score = (themScore + youScore);
+	var combinedScore = 0;
+	if (score < 6) {
+		combinedScore = 0;
+	}
+	else if (score < 11) {
+		combinedScore = 1;
+	}
+	else if (score < 16) {
+		combinedScore = 2;
+	}
+	else {
+		combinedScore = 3;
+	}
 	res.render('shared', {
 		"friendName": themName,
 		"friendScore": themScore,
 		"userScore": youScore,
+		"plantScore": combinedScore
 	})
 }
